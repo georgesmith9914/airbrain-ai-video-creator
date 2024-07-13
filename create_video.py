dd7718674bf6f2aaeb86293d2a61d00aa2b46633
George Smith <georgesmith9914@gmail.com>
2024-07-13 22:40:43 +0000

    updated

--- create_video.py
+++ create_video.py
@@ -14,6 +14,16 @@ import os
 import openai
 from openai import AzureOpenAI
 from moviepy.editor import *
+from PIL import Image
+import glob
+import cv2
+import numpy as np
+import requests
+from moviepy.editor import ImageClip, CompositeVideoClip, TextClip
+from moviepy.config import change_settings
+change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
+
+
 
 def generate_content(topic, num_of_sentences):
     openai.api_type = os.getenv("OPENAI_API_TYPE")
@@ -168,6 +178,111 @@ def get_image_phrases():
     print(image_phrases)
     return image_phrases
 
+def fetch_and_prepare_images(phrases, max_number_of_phrases):
+    print("Fetching and preparing images.....")
+    prompt = ''' Provide an image idea for each phrases: ''' + phrases
+    print(prompt)
+
+    import openai
+    from openai import AzureOpenAI
+    client = AzureOpenAI(
+        api_key = openai.api_key,
+        azure_endpoint = os.getenv("OPENAI_API_BASE"),
+        api_version = os.getenv("OPENAI_API_VERSION")
+        
+    )
+    deployment_name=os.getenv("MODEL_DEPLOYMENT_NAME")
+    # Assuming 'openai.api_key' is set elsewhere in your code or environment variables
+    completion = client.chat.completions.create(
+    model=deployment_name,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant that helps provide ideas for images based on key phrases. Do not use numbers for the list. Rewrite the prompt to be simpler and avoid any references to NSFW content, copyrighted characters, or controversial topics. When mentioning art styles, only include artists whose work predates 1912, or describe the style in general terms. Ensure the idea focuses on safe, universally acceptable themes without explicit, violent, or inappropriate content. Avoid political, controversial, or sensitive issues that might provoke or offend."},
+        {"role": "user", "content": prompt}
+    ]
+    )
+    print("Assistant: " + completion.choices[0].message.content)
+    response_phrase = completion.choices[0].message.content + ""
+
+    image_phrases = response_phrase.split("\n")[1:]
+    print(image_phrases)
+
+
+
+    # Base URL for the API call
+    base_url = os.getenv("PEXELS_IMAGES_API_BASE")
+
+    # Your API key
+    api_key = os.getenv("PEXELS_API_KEY")
+
+    # Headers for authorization
+    headers = {
+        "Authorization": api_key
+    }
+
+    image_counter = 0
+    # Loop through each phrase in the list
+    for phrase in image_phrases[:max_number_of_phrases]:
+        # Format the query parameter with the current phrase
+        params = {
+            "query": phrase,
+            "per_page": 1
+        }
+        
+        # Make the API call
+        response = requests.get(base_url, headers=headers, params=params)
+
+        #print(response)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Extract the image URL from the response
+            data = response.json()
+            if data['photos']:
+                image_url = data['photos'][0]['src']['original']
+                # Download the image
+                image_response = requests.get(image_url)
+                if image_response.status_code == 200:
+                    # Extract the file extension from the image URL
+                    _, extension = os.path.splitext(image_url)
+                    # Ensure the extension is not empty and has a valid image format; otherwise, default to '.jpg'
+                    if not extension in ['.jpg', '.jpeg', '.png', '.gif']:
+                        extension = '.jpg'
+                    # Save the image to a file with the correct extension
+                    #image_path = f"./images/{phrase.replace(' ', '_')}{extension}"
+                    image_path = f"./images/{image_counter}-generated_image{extension}"
+                    with open(image_path, 'wb') as file:
+                        file.write(image_response.content)
+                        # Open the JPEG file and convert it to RGB
+                    if extension == '.jpg' or extension == '.jpeg':
+                            # Open the PNG file and convert it to RGB
+                            im = Image.open(image_path).convert("RGB")
+                            # Create a .jpeg filename by replacing the .png extension
+                            png_file = image_path.rsplit('.', 1)[0] + '.png'
+                            # Save the image as a .jpeg
+                            im.save(png_file, "PNG")
+                            # Optionally, delete the original PNG file
+                            os.remove(image_path)
+                            image_path = png_file  # Update image_path to point to the new JPEG file
+
+                    print(f"Downloaded '{phrase}' image to {image_path}")
+                    image_counter += 1
+                else:
+                    print(f"Failed to download image for '{phrase}'")
+            else:
+                print(f"No photos found for '{phrase}'")
+        else:
+            print(f"Failed to fetch data for '{phrase}'")
+
+    # List all .webp files in the current directory
+    #webp_files = glob.glob('./prepare_images/*.webp')
+
+    #for webp_file in webp_files:
+        # Open the .webp image
+    #    im = Image.open(webp_file).convert("RGB")
+    #    # Create a .jpeg filename by replacing the .webp extension
+    #    jpeg_file = webp_file.rsplit('.', 1)[0] + '.jpg'
+        # Save the image as a .jpeg
+    #    im.save(jpeg_file, "JPEG")
 
 def generate_images(image_phrases, max_number_of_phrases):
     import requests
@@ -267,9 +382,33 @@ def generate_audio(content):
 
 #Stich the audio files and the images together
 
+def Zoom(clip,mode='in',position='center',speed=1):
+    fps = clip.fps
+    duration = clip.duration
+    total_frames = int(duration*fps)
+    def main(getframe,t):
+        frame = getframe(t)
+        h,w = frame.shape[:2]
+        i = t*fps
+        if mode == 'out':
+            i = total_frames-i
+        zoom = 1+(i*((0.1*speed)/total_frames))
+        positions = {'center':[(w-(w*zoom))/2,(h-(h*zoom))/2],
+                     'left':[0,(h-(h*zoom))/2],
+                     'right':[(w-(w*zoom)),(h-(h*zoom))/2],
+                     'top':[(w-(w*zoom))/2,0],
+                     'topleft':[0,0],
+                     'topright':[(w-(w*zoom)),0],
+                     'bottom':[(w-(w*zoom))/2,(h-(h*zoom))],
+                     'bottomleft':[0,(h-(h*zoom))],
+                     'bottomright':[(w-(w*zoom)),(h-(h*zoom))]}
+        tx,ty = positions[position]
+        M = np.array([[zoom,0,tx], [0,zoom,ty]])
+        frame = cv2.warpAffine(frame,M,(w,h))
+        return frame
+    return clip.fl(main)
 
-
-def create_video(images, audio, output):
+def create_video(images, audio, output, image_phrases):
     from PIL import Image
     from PIL import Image as pil
     from pkg_resources import parse_version
@@ -282,9 +421,28 @@ def create_video(images, audio, output):
     print("Creating the video.....")
 
 
-    clips = [ImageClip(m).resize(height=1024).set_duration(5) for m in images]
+    #clips = [ImageClip(m).resize(height=1024).set_duration(5) for m in images]
+    #clips = [Zoom(clip=ImageClip(m).resize(height=1024).set_duration(4).set_fps(30), mode='in', position='center', speed=1.2) for m in images]    
+    #clips = [Zoom(clip=ImageClip(m).resize(height=1024).set_duration(4).set_fps(30), mode='in' if i % 2 == 0 else 'out', position='center', speed=1.2) for i, m in enumerate(images)]
+    #print(clips)
 
-    print(clips)
+    # Assuming 'texts' is a list of text strings corresponding to each image in 'images'
+    clips = []
+    texts = image_phrases
+    for i, (m, text) in enumerate(zip(images, texts)):
+        # Create an image clip
+        img_clip = ImageClip(m).resize(height=1024).set_duration(4).set_fps(30)
+        
+        # Create a text clip (customize as needed)
+        txt_clip = TextClip(text, fontsize=70, color='white', font="Amiri-Bold", align='center').set_position('center').set_duration(4)
+        
+        # Overlay the text on the image
+        video_clip = CompositeVideoClip([img_clip, txt_clip])
+        
+        # Apply the Zoom effect with alternating mode
+        final_clip = Zoom(clip=video_clip, mode='in' if i % 2 == 0 else 'out', position='center', speed=1.2)
+        
+        clips.append(final_clip)
 
     concat_clip = concatenate_videoclips(clips, method="compose")
 
@@ -299,7 +457,7 @@ def create_video(images, audio, output):
     final_clip.write_videofile(output, fps=24)
 
 def stitch_video(image_phrases, max_number_of_phrases, filename):
-    print(len(image_phrases))
+    print(max_number_of_phrases)
 
     images = [f"images/{index}-generated_image.png" for index, _ in enumerate(image_phrases) if index < max_number_of_phrases]
     print(images)
@@ -310,13 +468,15 @@ def stitch_video(image_phrases, max_number_of_phrases, filename):
 
     output = "video.mp4"
 
-    create_video(images, audio, output)
+    create_video(images, audio, output, image_phrases)
     print("Video created.....")
     return output
 
 def main():
     print("Step 1: Generate the content")
-    content = generate_content("Healthy Eating", 5)
+    content = ""
+    #Topics: Books, Movies, Music, Health, Technology, Science, History, Art, Food, Travel, Research papers
+    content = generate_content("Arxiv Research Paper: Attention Is All You Need", 5)
     print("Content:", content)
 
     print("Step 2: Summarize the content")
@@ -326,16 +486,21 @@ def main():
     print("Summary:", summary)
 
     print("Step 3: Extract key phrases")
-    phrase_list = []
+    phrase_list = ['super healthy lifestyle', 'healthy eating', 'tasty fruits', 'good fats', 'chronic diseases', 'overall health', 'real boost', 'regular exercise', 'diverse diet', 'veggies']
     phrases = ""
     phrase_list, phrases = extract_key_phrases(summary)
     print("Key Phrases:", phrase_list)
     print("Phrases:", phrases)
 
-    print("Step 4: Generate images")
-    max_number_of_phrases = 4
+    print("Step 4a: Generate images")
+    max_number_of_phrases = 8
     generate_images(phrase_list, max_number_of_phrases)
     print("Images generated.....")
+
+    print("Step 4b: Fetch and Prepare images")
+    max_number_of_phrases = 8
+    #fetch_and_prepare_images(phrases, max_number_of_phrases)
+    print("Images prepared.....")
 
     print("Step 5: Generate audio")
     generate_audio(summary)
